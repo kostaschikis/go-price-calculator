1ed200710d43304a4f909da6d74f418a46c2d42a
Kostas Chikimtzis <kostaschikis@gmail.com>
2024-06-27 16:26:47 +0300

    outsourced the filemanager logic

--- filemanager/filemanager.go
+++ filemanager/filemanager.go
@@ -2,12 +2,18 @@ package filemanager
 
 import (
 	"bufio"
+	"encoding/json"
 	"errors"
 	"os"
 )
 
-func ReadLines(path string) ([]string, error) {
-	file, err := os.Open(path)
+type FileManager struct {
+	InputFilePath  string
+	OutputFilePath string
+}
+
+func (fm FileManager) ReadLines() ([]string, error) {
+	file, err := os.Open(fm.InputFilePath)
 
 	if err != nil {
 		return nil, errors.New("Failed to open file")
@@ -25,9 +31,36 @@ func ReadLines(path string) ([]string, error) {
 
 	if err != nil {
 		file.Close()
-		return nil, errors.New("Failed to read line in file")
+		return nil, errors.New("failed to read line in file")
 	}
 
 	file.Close()
 	return lines, nil
+}
+
+func (fm FileManager) WriteResult(data any) error {
+	file, err := os.Create(fm.OutputFilePath)
+
+	if err != nil {
+		return errors.New("failed to create file")
+	}
+
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(data)
+
+	if err != nil {
+		file.Close()
+		return errors.New("failed convert data to json")
+	}
+
+	file.Close()
+	return nil
+
+}
+
+func New(inputPath string, outputPath string) FileManager {
+	return FileManager{
+		InputFilePath:  inputPath,
+		OutputFilePath: outputPath,
+	}
 }
